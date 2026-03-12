c0ace0eef4e0b84f1afda9259a67bff8bf5aeeae
WildhanValerian25 <wildanvalerian89@gmail.com>
2026-03-12 23:29:38 +0700

    Menambahkan fungsi ExecuteScalar hitung mata kuliah

--- Form1.cs
+++ Form1.cs
@@ -62,7 +62,24 @@ namespace PraktikumADO
                 MessageBox.Show(ex.Message);
             }
         }
-
+        // Tambah fungsi ExecuteScalar hitung mata kuliah
+        private void btnHitungMK_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Koneksi();
+                conn.Open();
+                string query = "SELECT COUNT(*) FROM MataKuliah";
+                cmd = new SqlCommand(query, conn);
+                int jumlah = (int)cmd.ExecuteScalar();
+                txtHasil.Text = jumlah.ToString();
+                conn.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
         private void Form1_Load(object sender, EventArgs e)
         {
 
